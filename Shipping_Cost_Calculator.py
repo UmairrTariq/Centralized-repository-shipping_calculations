734bd8949a09c910556e77156998f89e16d46554
Umair tariq <tariqqumair68@gmail.com>
2025-11-14 01:07:18 -0500

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,5 @@ print(rate)
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Here is a new update by UmairrTariq
+print("You know I've already comepleted thi assignmenet in my local Vscode i thought it has to be done by ourselves in the start i see it as a task i did not know i had to do it all again.")
